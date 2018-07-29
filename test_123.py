7ddf3c1c088b5e44b4e2e6b31fb5dad621501df9
sayyid iskandar <sayyidkhan92@hotmail.com>
2018-07-29 12:22:22 +0800

    built a image viewer system

--- test_123.py
+++ test_123.py
@@ -1,22 +1,28 @@
-import tkinter as tk
+import tkinter as ttk
 #installing PIL, https://stackoverflow.com/questions/20060096/installing-pil-with-pip
 #pip3 install Pillow
 from PIL import Image, ImageTk
 
-window = tk.Tk()
+class image_test():
+    def __init__(self,image):
+        self.image = image
+    def getImage_txtFile(self):
+        return self.image
 
+window = tk.Tk()
 window.title("My First GUI")
 window.geometry("500x500")
 
 # Add your code to add widgets and
 #bind to events
-
-image = Image.open("images/" + "lenna.jpg")
+test = "lenna.jpg"
+image = Image.open("images/" + image_test(test).getImage_txtFile())
 photo = ImageTk.PhotoImage(image)
 
-label = tk.Label(image=photo)
-label.image = photo # keep a reference!
-label.pack()
+label1 = ttk.Label(image=photo)
+label1.image = photo # keep a reference!
+label1.pack()
+#label1.place(x=200, y=200)
 
 
 window.mainloop() #main loop to wait for events
